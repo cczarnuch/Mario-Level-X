226d5e7b5616a91dd9361e448d72315331ffa63f
edward <edwardliu321@gmail.com>
2020-01-21 22:06:14 -0500

    Added pipe class

--- src/data/components/collider.py
+++ src/data/components/collider.py
@@ -2,6 +2,7 @@ __author__ = 'justinarmstrong'
 
 import pygame as pg
 from .. import constants as c
+# from .. import setup
 
 class Collider(pg.sprite.Sprite):
     """Invisible sprites placed overtop background parts
@@ -15,3 +16,20 @@ class Collider(pg.sprite.Sprite):
         self.rect.y = y
         self.state = None
 
+class Pipe(pg.sprite.Sprite):
+    """Moveable Pipes"""
+    def __init__(self, x, height):
+        pg.sprite.Sprite.__init__(self)
+        #self.image = pg.Surface((83, height))
+        #self.image.fill(c.GREEN)
+        pipe_bottom = pg.image.load('./resources/graphics/pipe_bottom.png')
+        #self.image = pg.transform.scale(pipe_bottom, (83, height))
+        pipe_bottom = pg.transform.scale(pipe_bottom, (83,height))
+        #todo
+        pipe_top = pg.image.load('./resources/graphics/pipe_top.png')
+        pipe_top = pg.transform.scale(pipe_top, (83,40))
+        pipe_bottom.blit(pipe_top,(0,0))
+        self.image = pipe_bottom
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = 535 - height
