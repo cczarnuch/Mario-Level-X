89864f8f2b6001ae95ece8fc9c8b3a77c73c8ce2
edward <edwardliu321@gmail.com>
2020-01-26 15:53:11 -0500

    fixed step class

--- src/data/states/level1.py
+++ src/data/states/level1.py
@@ -69,7 +69,6 @@ class Level1(tools._State):
         self.viewport = setup.SCREEN.get_rect(bottom=self.level_rect.bottom)
         self.viewport.x = self.game_info[c.CAMERA_START_X]
 
-
     def setup_ground(self):
         """Creates collideable, invisible rectangles over top of the ground for
         sprites to walk on"""
@@ -105,41 +104,41 @@ class Level1(tools._State):
 
     def setup_steps(self):
         """Create collideable rects for all the steps"""
-        step1 = collider.Step(900, 495, 40, 44)
+        step1 = collider.Step(500, 350)
         
-        step1 = collider.Step(5745, 495, 40, 44)
-        step2 = collider.Step(5788, 452, 40, 44)
-        step3 = collider.Step(5831, 409, 40, 44)
-        step4 = collider.Step(5874, 366, 40, 176)
+        #step1 = collider.Step(5745, 495)
+        step2 = collider.Step(5788, 452)
+        step3 = collider.Step(5831, 409)
+        step4 = collider.Step(5874, 366)
 
 
-        step5 = collider.Step(6001, 366, 40, 176)
-        step6 = collider.Step(6044, 408, 40, 40)
-        step7 = collider.Step(6087, 452, 40, 40)
-        step8 = collider.Step(6130, 495, 40, 40)
+        step5 = collider.Step(6001, 366)
+        step6 = collider.Step(6044, 408)
+        step7 = collider.Step(6087, 452)
+        step8 = collider.Step(6130, 495)
 
-        step9 = collider.Step(6345, 495, 40, 40)
-        step10 = collider.Step(6388, 452, 40, 40)
-        step11 = collider.Step(6431, 409, 40, 40)
-        step12 = collider.Step(6474, 366, 40, 40)
-        step13 = collider.Step(6517, 366, 40, 176)
+        step9 = collider.Step(6345, 495)
+        step10 = collider.Step(6388, 452 )
+        step11 = collider.Step(6431, 409 )
+        step12 = collider.Step(6474, 366 )
+        step13 = collider.Step(6517, 366 )
 
-        step14 = collider.Step(6644, 366, 40, 176)
-        step15 = collider.Step(6687, 408, 40, 40)
-        step16 = collider.Step(6728, 452, 40, 40)
-        step17 = collider.Step(6771, 495, 40, 40)
+        step14 = collider.Step(6644, 366)
+        step15 = collider.Step(6687, 408)
+        step16 = collider.Step(6728, 452)
+        step17 = collider.Step(6771, 495)
 
-        step18 = collider.Step(7760, 495, 40, 40)
-        step19 = collider.Step(7803, 452, 40, 40)
-        step20 = collider.Step(7845, 409, 40, 40)
-        step21 = collider.Step(7888, 366, 40, 40)
-        step22 = collider.Step(7931, 323, 40, 40)
-        step23 = collider.Step(7974, 280, 40, 40)
-        step24 = collider.Step(8017, 237, 40, 40)
-        step25 = collider.Step(8060, 194, 40, 40)
-        step26 = collider.Step(8103, 194, 40, 360)
+        step18 = collider.Step(7760, 495)
+        step19 = collider.Step(7803, 452)
+        step20 = collider.Step(7845, 409)
+        step21 = collider.Step(7888, 366)
+        step22 = collider.Step(7931, 323)
+        step23 = collider.Step(7974, 280)
+        step24 = collider.Step(8017, 237)
+        step25 = collider.Step(8060, 194)
+        step26 = collider.Step(8103, 194)
 
-        step27 = collider.Step(8488, 495, 40, 40)
+        step27 = collider.Step(8488, 495)
 
         self.step_group = pg.sprite.Group(step1,  step2,
                                           step3,  step4,
@@ -164,7 +163,8 @@ class Level1(tools._State):
         self.powerup_group = pg.sprite.Group()
         self.brick_pieces_group = pg.sprite.Group()
 
-        brick1  = bricks.Brick(858,  365)
+        brick1  = bricks.Brick(500,  365)
+        #brick1  = bricks.Brick(858,  365)
         brick2  = bricks.Brick(944,  365)
         brick3  = bricks.Brick(1030, 365)
         brick4  = bricks.Brick(3299, 365)
@@ -1429,7 +1429,7 @@ class Level1(tools._State):
         self.coin_group.draw(self.level)
         self.brick_group.draw(self.level)
 
-        # self.pipe_group.draw(self.level)
+        self.pipe_group.draw(self.level)
         self.step_group.draw(self.level)
 
         self.coin_box_group.draw(self.level)
